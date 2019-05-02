12121606ad59eda1562dfda0bce708d056b07029
Cooper Holstein <cooperholstein@hotmail.com>
2019-05-01 20:45:55 -0700

    Improved homescreen and working settings menu

--- app/src/main/java/com/dream/somnipotent/MainActivity.java
+++ app/src/main/java/com/dream/somnipotent/MainActivity.java
@@ -1,11 +1,13 @@
 package com.dream.somnipotent;
 
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +25,15 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.home_screen_actions, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.settings:
+                startActivity(new Intent(this, Settings.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
